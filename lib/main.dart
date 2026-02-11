d122701c874db864fddae431ee6d66bc7ab9731b
paraZmol <nunes.20.24@gmail.com>
2026-02-11 18:03:05 -0500

    fix: corregir sintaxis en main dart eliminar gradle incrustado

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -102,7 +102,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
           // action in the IDE, or press "p" in the console), to see the
           // wireframe for each widget.
-          mainAxisAlignment: .center,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Text('You have pushed the button this many times:'),
             Text(
@@ -120,3 +120,16 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+buildscript {
+    repositories {
+        // ...existing code...
+    }
+    dependencies {
+        // ...existing code...
+        // Agregado para Google Services plugin (Firebase)
+        classpath 'com.google.gms:google-services:4.4.1'
+    }
+}
+
+// removed stray Gradle lines that were accidentally inserted into this Dart file
