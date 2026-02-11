62736f804a7cdf2ed09d4b8b3836e881e8e95ed2
paraZmol <nunes.20.24@gmail.com>
2026-02-11 17:21:29 -0500

    feat: configurar flavors android app_type cliente repartidor

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,20 +20,31 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.jla.glp2"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
+    // flavor dimension
+    flavorDimensions += "app_type"
+
+    productFlavors {
+        create("cliente") {
+            dimension = "app_type"
+            applicationId = "com.jla.cliente"
+            resValue("string", "app_name", "JLA Gas")
+        }
+        create("repartidor") {
+            dimension = "app_type"
+            applicationId = "com.jla.repartidor"
+            resValue("string", "app_name", "JLA Conductor")
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
